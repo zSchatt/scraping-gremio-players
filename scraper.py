92f9f499474bb3893f88a4fd15824564324ecae9
zSchatt <72706376+zSchatt@users.noreply.github.com>
2021-10-25 09:04:21 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -12,6 +12,10 @@ driver = webdriver.Firefox()
 
 driver.get(url)
 
+
+
+
+
 # COLETANDO INFO DOS JOGADORES
 driver.find_element_by_xpath('//*[@id="fittPageContainer"]/div[2]/div[5]/div/div/section/div/section/div[4]/div[1]/div[2]/div/div[2]/table')
 elementG = driver.find_element_by_xpath('//*[@id="fittPageContainer"]/div[2]/div[5]/div/div/section/div/section/div[4]/div[1]/div[2]/div/div[2]/table')
@@ -21,12 +25,20 @@ driver.find_element_by_xpath('//*[@id="fittPageContainer"]/div[2]/div[5]/div/div
 elementL = driver.find_element_by_xpath('//*[@id="fittPageContainer"]/div[2]/div[5]/div/div/section/div/section/div[4]/div[2]/div[2]/div/div[2]/table')
 html_contentL = elementL.get_attribute('outerHTML')
 
+
+
+
+
 # HTML JOGADORES
 soupG = BeautifulSoup(html_contentG, 'html.parser')
 tableG = soupG.find(name='table')
 
 soupL = BeautifulSoup(html_contentL, 'html.parser')
 tableL = soupL.find(name='table')
+
+
+
+
 
 # TABELAS INFORMAÇÃO DOS JOGADORES
 dfG_full = pd.read_html(str(tableG))[0]
@@ -36,6 +48,11 @@ dfG.columns = ['Nome', 'Posição', 'Idade', 'Altura', 'Peso', 'Nacionalidade']
 dfL_full = pd.read_html(str(tableL))[0]
 dfL = dfL_full[['Nome', 'POS', 'Idade', 'Alt', 'P', 'NAC']]
 dfL.columns = ['Nome', 'Posicao', 'Idade', 'Altura', 'Peso', 'Nacionalidade']
+
+
+
+
+
 
 #VARIÁVEIS DICIONÁRIO
 info_jogadoresGol = {}
@@ -47,7 +64,17 @@ info_jogadoresLinha['info'] = dfL.to_dict('records')
 info_jogadores = {}
 info_jogadores['Jogadores'] = dfG.to_dict('records') + dfL.to_dict('records')
 
+
+
+
+
+
 driver.quit()
+
+
+
+
+
 
 # CRIAÇÃO DO JSON
 js = json.dumps(info_jogadores)
